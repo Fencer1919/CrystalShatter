4c30dcb529dcb0f51fb657bff791b1d1fa2bbb3a
Mert Aksoy <mert@ekipmobilya.com>
2023-01-22 20:06:14 +0300

    Board Script Advanced, Board Instantiated On Start
    Board Script Advanced, Board Instantiated On Start. Grids, tiles of board are instantiated on start.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,32 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class Board : MonoBehaviour
 {
     [SerializeField] private int width;
     [SerializeField] private int height;
 
+    // One grid, one tile prefab
     [SerializeField] private GameObject tilePrefab;
 
-    Tile[,] m_allTiles;
-    
+    // Two dimension array created with [,]
+    Tile [,] allTiles;
+
     // Start is called before the first frame update
     void Start()
     {
-        m_allTiles = new Tile[width, height];
+        // Initializing allTiles array and instantiating the tiles
+        allTiles = new Tile [width, height];
         SetupTiles();
     }
     
     void SetupTiles()
     {
-        for (int i = 0; i < width; i++) 
+        // Nested for loops
+        // After creating the first column, this for makes it go to second row and instantiate the second column
+        for (int i = 0; i < width; i++)
         {
-            for (int j = 0; j < height; j++) 
+            // This creates, instantiates the tile prefab for the whole column first
+            for (int j = 0; j < height; j++)
             {
                 GameObject tile = Instantiate (tilePrefab, new Vector3(i, j, 0), Quaternion.identity) as GameObject;
 
+                // Going to be researched for comments
+                tile.name = "Tile (" + i + "," + j + ")";
 
+                allTiles[i, j] = tile.GetComponent<Tile>();
+
+                tile.transform.parent = transform;
 
 
             }
