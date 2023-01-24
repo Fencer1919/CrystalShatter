153afad2f722ca28caf5f19b1ec76c5aeaaa51b9
Fencer2021 <mert@ekipmobilya.com>
2023-01-24 18:44:37 +0300

    Game Piece Movement Script Added
    Game Piece Movement Script Added

--- Assets/Scripts/GamePiece.cs
+++ Assets/Scripts/GamePiece.cs
@@ -7,7 +7,9 @@ public class GamePiece : MonoBehaviour
 {
     public int xIndex;
     public int yIndex;
-    
+
+    private bool isMoving;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +26,52 @@ public class GamePiece : MonoBehaviour
     {
         xIndex = x;
         yIndex = y;
+    }
+
+    // X and Y coordinates of the destination and the time of the movement action
+    public void Move (int destX, int destY, float movementTime)
+    {
+        if (!isMoving)
+        {
+            StartCoroutine(MoveRoutine(new Vector3(destX, destY, 0), movementTime));
+        }
+    }
+
+    IEnumerator MoveRoutine(Vector3 destination, float movementTime)
+    {
+        Vector3 startPosition = transform.position;
+
+        bool reachedDestination = false;
+
+        float elapsedTime = 0f;
+
+        isMoving = true;
+
+        while (!reachedDestination)
+        {
+            if (Vector3.Distance(transform.position, destination) < 0.01f)
+            {
+                reachedDestination = true;
+                transform.position = destination;
+                SetCoordinate((int)destination.x, (int)destination.y);
+                break;
+
+            }
+
+            elapsedTime += Time.deltaTime;
+
+            float t = Mathf.Clamp(elapsedTime / movementTime, 0f, 1f);
+
+            transform.position = Vector3.Lerp(startPosition, destination, t);
+
+
+
+            // Wait until next frame
+            yield return null;
+        }
+
+        isMoving = false;
 
     }
+
 }
